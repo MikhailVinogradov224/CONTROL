e84ec001ef17adfa4c15e61483875d00d34643ee
MikhailVinogradov224 <m89119750476@gmail.com>
2022-06-19 04:20:45 +0300

    some changes

--- Program.cs
+++ Program.cs
@@ -2,12 +2,14 @@
  
 string[] array ={ "a", "bb", "ccc", "dddd", "eeeee", "z"};
 var result = new string[array.Length];
-var realSize = 0;
+var sizeElementa = 0;
 foreach (var value in array)
 {
     if (value.Length <= 3)
     {
-        result[realSize] = value;
-        realSize++;
+        result[sizeElementa] = value;
+        sizeElementa++;
     }
 }
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, sizeElementa));
