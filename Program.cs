bccfee4d165f6cae703ca8a0a950e75fe8f6ff39
MikhailVinogradov224 <m89119750476@gmail.com>
2022-06-19 04:16:17 +0300

    sozdanie metoda

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
  
 string[] array ={ "a", "bb", "ccc", "dddd", "eeeee", "z"};
+var result = new string[array.Length];
+var sizeElementa = 0;
+foreach (var value in array)
+{
+
+}
  
