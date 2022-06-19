e2a3f75088749b39d35bceb41ca581bcf7ca08f3
MikhailVinogradov224 <m89119750476@gmail.com>
2022-06-19 04:17:04 +0300

    some changes

--- Program.cs
+++ Program.cs
@@ -2,9 +2,12 @@
  
 string[] array ={ "a", "bb", "ccc", "dddd", "eeeee", "z"};
 var result = new string[array.Length];
-var sizeElementa = 0;
+var realSize = 0;
 foreach (var value in array)
 {
-
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
 }
- 
