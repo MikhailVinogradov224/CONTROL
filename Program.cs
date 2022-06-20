ecb9e43d2b3b4b8de47bfc9c61a0ee56cd4032b1
MikhailVinogradov224 <m89119750476@gmail.com>
2022-06-20 11:16:37 +0300

    some changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
- 
-string[] array ={ "a", "12", "dcae12", "ПриветЗдравствуйте", "21389", "K"};
+﻿string[] array ={ "a", "12", "dcae12", "ПриветЗдравствуйте", "21389", "K"};
 var result = new string[array.Length];
 var sizeElementa = 0;
 foreach (var value in array)
